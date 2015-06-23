b8e1a178cffda2093f06bf14effa4de7aa8da0b3
CIAN GALLAGHER <cg1830@gmail.com>
2015-06-24 00:12:06 +0100

    Added test server PHP file

---
+++ server/server.php
@@ -0,0 +1,10 @@
+<?php
+
+if (isset($_FILES['imagedata'])) {
+    $file = "images/" . substr(md5(rand() . time()), 0, 13) . ".png";
+    $open = fopen($file, 'w');
+    fclose($open);
+    move_uploaded_file($_FILES['imagedata']['tmp_name'], $file);
+
+    echo "http://broconut.com/" . $file;
+}
