aff7cb871bd99b1b0b4b80dc75a632a34a4c5339
CIAN GALLAGHER <cg1830@gmail.com>
2015-06-29 02:10:43 +0100

    Added settings class which consists of a frame to allow input to be made and set to a properties file

--- src/broconut/ciangallagher/net/TraySettings.java
+++ src/broconut/ciangallagher/net/TraySettings.java
@@ -1,7 +1,38 @@
 package broconut.ciangallagher.net;
 
+
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * Created by Cian on 28/06/2015.
  */
-public class TraySettings {
+class TraySettings {
+
+    private final static String BUTTON = "Button 1";
+    private final static String TEXT1 = "This is some sample text";
+
+    public TraySettings () {
+        // set the look and feel of the window
+        try {
+            UIManager.setLookAndFeel(UIManager.getCrossPlatformLookAndFeelClassName());
+        } catch (Exception w) {w.printStackTrace();}
+
+        JFrame frame = new JFrame();
+
+        JPanel pMain;
+        JPanel p1 = new JPanel();
+        JPanel p2 = new JPanel();
+
+        pMain = new JPanel(new CardLayout());
+        pMain.add(p1, BUTTON);
+        pMain.add(p2, TEXT1);
+
+        frame.setSize(400,300);
+        frame.setLocationRelativeTo(null);
+        // dispose of window only on close
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+    }
+
 }
